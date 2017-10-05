96d75f94f2de5a851b7ea149c6e3f21702c9c9fe
uvsq21404240 <thomas.momenzadeh@gmail.com>
2017-10-05 09:08:00 +0200

    complet rien a changer

---
+++ Junit/src/Comptebancaire.java
@@ -0,0 +1,41 @@
+
+public class Comptebancaire {
+
+	private int solde ;
+	public Comptebancaire(int s)
+	{
+		this.solde = s;
+	}
+	
+	public int getsolde()
+	{
+		return this.solde;
+	}
+	
+	
+	public void credit(int v)
+	{
+		this.solde = this.solde + v ;
+	}
+	public void debit(int v)
+	{
+		if(this.solde >= v && v>0 )
+		{
+		this.solde = this.solde - v;
+		}
+	}
+	public void virement(Comptebancaire c , int v)
+	{
+		if(this.solde >= v && v>0 )
+		{
+		///c.solde = c.solde + v;
+			c.credit(v);
+		///this.solde = this.solde - v;
+			this.debit(v);
+		}
+	}
+	
+}
+
+
+
